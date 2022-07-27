8a10aad67129999058356c411aee8a78ef2690fd
chienvmo <chiennv1@vmodev.com>
2022-07-27 17:20:58 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { Button } from "@mui/material";
+import Header from "./components/Header";
+function App() {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header/index.jsx
@@ -0,0 +1,62 @@
+import { AccountCircle, Bookmark, Search } from "@mui/icons-material";
+import Button from "@mui/material/Button";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+import TextField from "@mui/material/TextField";
+import { useState } from "react";
+import SignUp from "../../features/Auth/SignUp";
+function Header() {
+  const [open, setOpen] = useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+  return (
+    <div className='flex justify-around items-center bg-[#1b2d3c] h-[65px] text-white'>
+      <div>
+        <img
+          className='w-[100px] h-[30px]'
+          src='https://rubee.com.vn/admin/webroot/upload/image/images/logo-mgm.jpg'
+        />
+      </div>
+      <div className='bg-[#273e52] px-[40px] py-[10px] rounded-[20px]'>
+        <Search />
+        <input
+          className='bg-[#273e52] outline-none px-3'
+          type='text'
+          placeholder='Tim kiem'
+        />
+      </div>
+      <button className='bg-gradient-to-r from-cyan-900 to-blue-700 rounded-[20px] px-[15px] py-[6px]'>
+        <Bookmark />
+        <span className='px-4'>Phim yeu thich</span>
+      </button>
+      <button
+        onClick={handleClickOpen}
+        className='bg-gradient-to-r from-cyan-900 to-blue-700 rounded-[20px] px-[15px] py-[6px]'
+      >
+        <AccountCircle />
+        <span className='px-4'>Dang nhap</span>
+      </button>
+      <Dialog open={open} onClose={handleClose}>
+        <DialogTitle>Subscribe</DialogTitle>
+        <DialogContent>
+          <SignUp />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Cancel</Button>
+          <Button onClick={handleClose}>Subscribe</Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+}
+
+export default Header;
